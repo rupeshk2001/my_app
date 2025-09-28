e99a08c1e4ea67e7eeab85d41d3c601a1df54114
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2025-09-28 15:19:10 +0530

    home page demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home/home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit THis is Commit test 1st new Clone <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +20,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <Home />
       </header>
     </div>
   );
